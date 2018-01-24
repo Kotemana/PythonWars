6889a85497d79ab9eaea2dbe1091a857f7e13a39
Leonid Radkevich <laradkevich@gmail.com>
2018-01-24 22:02:09 +0300

    Refactoring

--- helloworld.py
+++ helloworld.py
@@ -1,15 +1,12 @@
-import die
 import monster_desc
 
 monster = monster_desc.Monster("Orc", 100, 2, 2, 6)
 hero = monster_desc.Monster("Paladin", 50, 5, 2, 8)
 
-
-
-while(True):
-    if(monster_desc.fight(hero, monster)):
+while True:
+    if monster_desc.fight(hero, monster):
         break
-    if (monster_desc.fight(monster, hero)):
+    if monster_desc.fight(monster, hero):
         break
 
 
--- monster_desc.py
+++ monster_desc.py
@@ -1,37 +1,39 @@
 from die import *
 
+
 def fight(attacker, receiver):
-    if(attacker.check_hit_another(receiver)):
-        print(attacker.name, "hits", receiver.name, "for", attacker.hit_another(receiver), "hp. Now", \
+    if attacker.check_hit_another(receiver):
+        print(attacker.name, "hits", receiver.name, "for", attacker.hit_another(receiver), "hp. Now",
               receiver.name, "is", receiver.hp)
     else:
         print(attacker.name, "misses", receiver.name)
-    if (receiver.is_dead):
+    if receiver.is_dead:
         print(receiver.name, "is dead!", attacker.name, "wins")
         return True
     return False
+
 
 class Monster:
 
     def __init__(self, name, hp, armor, ap_throws, ap_sides):
         self.attack_power = AttackPower(ap_throws, ap_sides)
-        self.name=name
-        self.hp=hp
-        self.armor=armor
+        self.name = name
+        self.hp = hp
+        self.armor = armor
 
     @property
     def is_dead(self):
-        return self.hp<0
+        return self.hp < 0
 
-    def check_hit_another(self, another):
+    @staticmethod
+    def check_hit_another(another):
         """
         :param another: is monster
         :returns bool
         """
 
-
-        die=Die(20)
-        return die.roll(1)>5+another.armor
+        die = Die(20)
+        return die.roll(1) > 5 + another.armor
 
     def hit_another(self, another):
         """
@@ -39,12 +41,10 @@ class Monster:
         :returns bool
         """
 
-
-        die=Die(self.attack_power.sides)
-        result=die.roll(self.attack_power.throws)
-        another.hp-=result
+        die = Die(self.attack_power.sides)
+        result = die.roll(self.attack_power.throws)
+        another.hp -= result
         return result
-
 
 
 class AttackPower:
