3c22e0e2d1bfda5787c5d1c8032624f7fcb10437
Leonid Radkevich <laradkevich@gmail.com>
2018-02-05 20:38:10 +0300

    Added weapons with types and conditions

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,8 @@
 import monster_desc
-
-monster = monster_desc.Monster("Orc", 100, 2, 2, 6)
-hero = monster_desc.Monster("Paladin", 50, 5, 2, 8)
+dagger = monster_desc.Weapon(1, 6, "dagger", monster_desc.WeaponType.PIERCE)
+warhammer = monster_desc.Weapon(1, 8, "warhammer", monster_desc.WeaponType.BLUNT)
+monster = monster_desc.Monster("Orc", 100, 2, dagger)
+hero = monster_desc.Monster("Paladin", 50, 10)
 
 while True:
     if monster_desc.fight(hero, monster):
--- monster_desc.py
+++ monster_desc.py
@@ -1,12 +1,18 @@
 from die import *
+from enum import Enum
 
 
 def fight(attacker, receiver):
-    if attacker.check_hit_another(receiver):
-        print(attacker.name, "hits", receiver.name, "for", attacker.hit_another(receiver), "hp. Now",
-              receiver.name, "is", receiver.hp)
+    if not attacker.isStunned:
+        if attacker.check_hit_another(receiver):
+            print(attacker.name, "hits", receiver.name, "for", attacker.hit_another(receiver), "hp with his ",
+                  attacker.weapon.name, ". Now",
+                  receiver.name, "is", receiver.hp)
+        else:
+            print(attacker.name, "misses", receiver.name)
     else:
-        print(attacker.name, "misses", receiver.name)
+        print(attacker.name, "is stunned and misses his chance to attack")
+        attacker.isStunned = False
     if receiver.is_dead:
         print(receiver.name, "is dead!", attacker.name, "wins")
         return True
@@ -14,36 +20,45 @@ def fight(attacker, receiver):
 
 
 class Monster:
-
-    def __init__(self, name, hp, armor, ap_throws, ap_sides):
-        self.attack_power = AttackPower(ap_throws, ap_sides)
+    def __init__(self, name, hp, armor, weapon=None):
+        if weapon is None:
+            self.weapon = Weapon(1, 2, "bare hands", WeaponType.BLUNT)
+        else:
+            self.weapon = weapon
         self.name = name
         self.hp = hp
         self.armor = armor
+        self.isStunned = False
 
     @property
     def is_dead(self):
         return self.hp < 0
 
-    @staticmethod
-    def check_hit_another(another):
+    def check_hit_another(self, another):
         """
         :param another: is monster
         :returns bool
         """
 
         die = Die(20)
-        return die.roll(1) > 5 + another.armor
+        to_hit = 5 + another.armor
+        if to_hit > 10 and self.weapon.weaponType == WeaponType.PIERCE:
+            to_hit = 10
+        return die.roll(1) > to_hit
 
     def hit_another(self, another):
         """
         :param another: is monster
         :returns bool
         """
-
-        die = Die(self.attack_power.sides)
-        result = die.roll(self.attack_power.throws)
+        die = Die(self.weapon.attack_power.sides)
+        result = die.roll(self.weapon.attack_power.throws)
         another.hp -= result
+        if self.weapon.weaponType == WeaponType.BLUNT:
+            stun_die = Die(5)
+            if stun_die.roll(1) == 5:
+                another.isStunned = True
+                print(another.name, "is stunned by mighty blow of", self.weapon.name)
         return result
 
 
@@ -51,3 +66,16 @@ class AttackPower:
     def __init__(self, ap_throws, ap_sides):
         self.sides = ap_sides
         self.throws = ap_throws
+
+
+class Weapon:
+    def __init__(self, ap_throws, ap_sides, name, weapon_type):
+        self.attack_power = AttackPower(ap_throws, ap_sides)
+        self.name = name
+        self.weaponType = weapon_type
+
+
+class WeaponType(Enum):
+    PIERCE = 1
+    BLUNT = 2
+    SLASH = 3
